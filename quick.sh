be90ac998ec16760d8e72cd854d5e8ec081910f6
Edson Araujo <edson.araujo@opensea.us>
2020-10-16 16:58:23 -0400

    Try again

--- quick.sh
+++ quick.sh
@@ -3,11 +3,12 @@
 # Generally follows the Travis CI process stages.
 
 # Optionally generate new certs
-./make_cert.sh
+
+# ./make_cert.sh
 pylint src/*.py
 bandit src/*.py --skip B101
 pytest src/test_unit.py
 sudo docker-compose down --volumes
 sudo docker-compose up --build --detach
 ./wait_for_https.sh 60
-# pytest src/test_system.py
+pytest src/test_system.py
